eeb6c6fd199fa488e63a7b783e17320f42ab50b4
Englebabz <boye.esben@gmail.com>
2014-01-27 17:25:53 +0100

    jan26

---
+++ Data/CasPyTexConfig.py
@@ -0,0 +1,4 @@
+Use_Coloredoutput=True
+Color_of_output="red"
+Use_Radians=False
+Decimal_Places=6
--- Data/sandbox.py
+++ Data/sandbox.py
@@ -1,1 +1,2 @@
-print("forget a"[6:])
+f=open(r"C:\Users\Esben\home\CAS-v3\Data\Bestemmelse af stamfunktion med tangent.cpt")
+print(f.readlines())
